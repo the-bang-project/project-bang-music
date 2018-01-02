396b91534f97d4e146dad3ee43c1995266f71bfc
Jamalhalili03 <Jamalhalili03@gmail.com>
2018-01-02 21:40:37 +0000

    Settings Activity
    Settings activity added, but not connected to any buttons.

---
+++ app/src/main/java/com/thebangproject/music/bangmusic/SettingsFragment.java
@@ -0,0 +1,26 @@
+package com.thebangproject.music.bangmusic;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.support.v7.preference.PreferenceFragmentCompat;
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+
+
+
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState,
+                                    String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+
+}
