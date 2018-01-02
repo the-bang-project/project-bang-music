aeec1ff3197d6727235f8e875b5ec25c6fb51f68
Dylan Neve <dylanneve1@users.noreply.github.com>
2018-01-02 20:52:14 +0000

    Add new three dot menu
    - Includes website and settings which will replace other settings in the future.

--- app/src/main/java/com/thebangproject/music/bangmusic/MainActivity.java
+++ app/src/main/java/com/thebangproject/music/bangmusic/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -44,4 +45,9 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.dotmenu, menu);
+        return true;
+    }
 }
