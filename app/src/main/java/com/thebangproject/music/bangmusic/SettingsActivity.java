cb08ab65989249bae84a1744d5c66641a66ddc0b
Dylan Neve <dylanneve1@users.noreply.github.com>
2018-01-03 22:43:40 +0000

    Button didnt work

--- app/src/main/java/com/thebangproject/music/bangmusic/SettingsActivity.java
+++ app/src/main/java/com/thebangproject/music/bangmusic/SettingsActivity.java
@@ -1,10 +1,13 @@
 package com.thebangproject.music.bangmusic;
 
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Switch;
 import android.widget.CompoundButton;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -14,27 +17,6 @@ public class SettingsActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //Get widgets reference from XML layout
-        final TextView tView = (TextView) findViewById(R.id.tv);
-        Switch sButton = (Switch) findViewById(R.id.nightSwitch);
-
-        //Set a CheckedChange Listener for Switch Button
-        sButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton cb, boolean on) {
-                if (on) {
-                    //Do something when Switch button is on/checked
-                    tView.setText("Switch is on.....");
-                } else {
-                    //Do something when Switch is off/unchecked
-                    tView.setText("Switch is off.....");
-                }
-            }
-        });
     }
 }
 
