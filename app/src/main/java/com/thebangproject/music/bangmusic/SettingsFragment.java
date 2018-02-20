b085c0bf3601ee716caaa9faafcea603ff03f87b
Dylan Neve <dylanneve1@users.noreply.github.com>
2018-02-20 21:45:35 +0000

    Our Website*

--- app/src/main/java/com/thebangproject/music/bangmusic/SettingsFragment.java
+++ app/src/main/java/com/thebangproject/music/bangmusic/SettingsFragment.java
@@ -13,10 +13,6 @@ import android.support.v7.preference.PreferenceFragmentCompat;
  */
 public class SettingsFragment extends PreferenceFragmentCompat {
 
-
-
-
-
     @Override
     public void onCreatePreferences(Bundle savedInstanceState,
                                     String rootKey) {
