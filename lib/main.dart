9f74fb9f0f7fd097ba897f123dcec136b1fcc2f1
Dylan Neve <dylanneve1@users.noreply.github.com>
2018-06-08 17:56:18 +0100

    Add new pages that actually work in the hamburger menu.

--- lib/main.dart
+++ lib/main.dart
@@ -37,19 +37,91 @@ class _MyHomePageState extends State<MyHomePage> {
               new DrawerHeader(child: new Text('Bang Music'),),
               new ListTile(
                 title: new Text('My Library'),
-                onTap: () {},
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    new MaterialPageRoute(builder: (context) => new MyLibrary())
+                  );
+                },
               ),
               new ListTile(
                 title: new Text('Search'),
-                onTap: () {},
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    new MaterialPageRoute(builder: (context) => new SearchScreen())
+                  );
+                },
               ),
               new Divider(),
               new ListTile(
                 title: new Text('About'),
-                onTap: () {},
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    new MaterialPageRoute(builder: (context) => new AboutScreen())
+                  );
+                },
               ),
             ],
           )
+      ),
+    );
+  }
+}
+
+class MyLibrary extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("My Library"),
+      ),
+      body: new Center(
+        child: new RaisedButton(
+          onPressed: () {
+            // Navigate back to first screen when tapped!
+          },
+          child: new Text('This is your library.'),
+        ),
+      ),
+    );
+  }
+}
+
+class SearchScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Search"),
+      ),
+      body: new Center(
+        child: new RaisedButton(
+          onPressed: () {
+            // Navigate back to first screen when tapped!
+          },
+          child: new Text('This is your search page.'),
+        ),
+      ),
+    );
+  }
+}
+
+class AboutScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("About"),
+      ),
+      body: new Center(
+        child: new RaisedButton(
+          onPressed: () {
+            // Navigate back to first screen when tapped!
+          },
+          child: new Text('(About us)'),
+        ),
       ),
     );
   }
